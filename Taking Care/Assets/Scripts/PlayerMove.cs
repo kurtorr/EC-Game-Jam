9888d3df22ef8ef175aec496cc68690e9a6ead41
kurtorr <kurtorr2017@gmail.com>
2020-08-21 17:40:32 -0400

    updated unity

--- Taking Care/Assets/Scripts/PlayerMove.cs
+++ Taking Care/Assets/Scripts/PlayerMove.cs
@@ -12,12 +12,6 @@ public class PlayerMove : MonoBehaviour
     public Rigidbody2D rigidbody2d;
     public LayerMask groundLayerMask;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
