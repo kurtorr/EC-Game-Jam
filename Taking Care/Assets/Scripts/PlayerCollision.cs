823a18808b7e4dc9f83f89da9e47215e18d0ba07
kurtorr <kurtorr2017@gmail.com>
2020-08-22 11:40:48 -0400

    Lighting

---
+++ Taking Care/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        SceneManager.LoadScene("SampleScene");
+    }
+}
