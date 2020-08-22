b21cfe9b87d66d689d8018aef1b00c3a5d8b6f8e
kurtorr <kurtorr2017@gmail.com>
2020-08-22 18:29:19 -0400

    Started Animation Transitions
    Created transition variables
    Connected transitions from Take to Take_Jump, _Run, and _Crouch
    Started working on animation transition script

---
+++ Taking Care/Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    //
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D)))
+        {
+            anim.SetBool("playerIsWalk", true);
+        }
+        else
+        {
+            anim.SetBool("playerIsWalk", false);
+        }
+    }
+}
